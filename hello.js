035dfd836707264c1c661f28313fe8c5621196e8
RacharlaSanjana <114012057+RacharlaSanjana@users.noreply.github.com>
2023-06-20 12:52:28 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,28 @@
+function myFunction() {
+    var x = document.getElementById("myText").value;
+    var z = document.getElementById("text").value;
+    var w = document.getElementById("email").value;
+    var l = document.getElementById("pincode").value;
+    var s= document.getElementById("Gen").value;
+    var d= document.getElementById("degree_type").value;
+    var b= document.getElementById("branch").value;
+    var c= document.getElementById("course").value;
+    var g= document.getElementById("gra").value;
+
+    document.getElementById("san").innerHTML = "Registered Student Details";
+    document.getElementById("demo").innerHTML = x;
+    document.getElementById("gen").innerHTML = s;
+    document.getElementById("add").innerHTML = z;
+    document.getElementById("emai").innerHTML = w;
+    document.getElementById("pin").innerHTML = l;
+    document.getElementById("deg").innerHTML = d;
+    document.getElementById("grad").innerHTML = g;
+    document.getElementById("bran").innerHTML = b;
+    document.getElementById("cour").innerHTML = c;
+
+    
+    
+}
+
+
+
